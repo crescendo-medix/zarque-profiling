434215089b04c409c78b4c5c1e89c9d1e5a6ccc2
shiolemon <132550577+crescendo-medix@users.noreply.github.com>
2023-05-11 00:27:25 +0900

    Add files via upload

---
+++ src/zarque_profiling/report/presentation/flavours/widget/__init__.py
@@ -0,0 +1,49 @@
+from zarque_profiling.report.presentation.flavours.widget.alerts import WidgetAlerts
+from zarque_profiling.report.presentation.flavours.widget.collapse import WidgetCollapse
+from zarque_profiling.report.presentation.flavours.widget.container import (
+    WidgetContainer,
+)
+from zarque_profiling.report.presentation.flavours.widget.correlation_table import (
+    WidgetCorrelationTable,
+)
+from zarque_profiling.report.presentation.flavours.widget.dropdown import WidgetDropdown
+from zarque_profiling.report.presentation.flavours.widget.duplicate import (
+    WidgetDuplicate,
+)
+from zarque_profiling.report.presentation.flavours.widget.frequency_table import (
+    WidgetFrequencyTable,
+)
+from zarque_profiling.report.presentation.flavours.widget.frequency_table_small import (
+    WidgetFrequencyTableSmall,
+)
+from zarque_profiling.report.presentation.flavours.widget.html import WidgetHTML
+from zarque_profiling.report.presentation.flavours.widget.image import WidgetImage
+from zarque_profiling.report.presentation.flavours.widget.root import WidgetRoot
+from zarque_profiling.report.presentation.flavours.widget.sample import WidgetSample
+from zarque_profiling.report.presentation.flavours.widget.table import WidgetTable
+from zarque_profiling.report.presentation.flavours.widget.toggle_button import (
+    WidgetToggleButton,
+)
+from zarque_profiling.report.presentation.flavours.widget.variable import WidgetVariable
+from zarque_profiling.report.presentation.flavours.widget.variable_info import (
+    WidgetVariableInfo,
+)
+
+__all__ = [
+    "WidgetCollapse",
+    "WidgetContainer",
+    "WidgetDuplicate",
+    "WidgetDropdown",
+    "WidgetFrequencyTable",
+    "WidgetFrequencyTableSmall",
+    "WidgetHTML",
+    "WidgetImage",
+    "WidgetRoot",
+    "WidgetSample",
+    "WidgetTable",
+    "WidgetToggleButton",
+    "WidgetVariable",
+    "WidgetVariableInfo",
+    "WidgetAlerts",
+    "WidgetCorrelationTable",
+]
